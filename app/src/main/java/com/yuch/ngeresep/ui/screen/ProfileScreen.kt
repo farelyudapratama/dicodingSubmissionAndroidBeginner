2f395adca0cb0cd2e7066a819c927f9d0c2b9960
farelyudapratama <dhakayudha2@gmail.com>
2024-07-28 11:42:02 +0700

    2-

--- app/src/main/java/com/yuch/ngeresep/ui/screen/ProfileScreen.kt
+++ app/src/main/java/com/yuch/ngeresep/ui/screen/ProfileScreen.kt
@@ -1,20 +1,15 @@
 package com.yuch.ngeresep.ui.screen
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,6 +18,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,41 +33,23 @@ fun ProfileScreen(
     modifier: Modifier = Modifier
 ) {
     Box(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = modifier.padding(16.dp)
         ) {
-            Box(modifier = Modifier.size(250.dp)) {
-                Image(
-                    painter = painterResource(id = user.foto),
-                    contentDescription = "Photo Profile",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(250.dp)
-                        .clip(RoundedCornerShape(8.dp))
-                )
-                IconButton(
-                    onClick = { /* Implement edit functionality here */ },
-                    modifier = Modifier
-                        .align(Alignment.TopEnd)
-                        .offset(x = 16.dp, y = -16.dp)
-                        .padding(4.dp)
-                        .size(40.dp)
-                        .clip(CircleShape)
-                        .background(Color.White)
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Edit,
-                        contentDescription = "Edit Photo",
-                        tint = Color.Gray,
-                        modifier = Modifier
-                            .size(24.dp)
-                    )
-                }
-            }
+            Image(
+                painter = painterResource(id = user.foto),
+                contentDescription = "Photo Profile",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(250.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )
             Text(
                 text = user.nama,
                 modifier = Modifier
@@ -80,7 +58,6 @@ fun ProfileScreen(
                 fontSize = 26.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center,
-                color = Color.Black,
             )
             Text(
                 text = user.email,
@@ -90,20 +67,7 @@ fun ProfileScreen(
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center,
-                color = Color.Black,
             )
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ProfilePreview() {
-    ProfileScreen(
-        user = User(
-            nama = "Farel Yuda Pratama",
-            email = "farelyudapratama51@gmail.com",
-            foto = R.drawable.poto
-        )
-    )
 }
